596fcf1d63de504677c89743dc875b075a532c33
Ubuntu <ubuntu@ip-172-31-40-92.us-east-2.compute.internal>
2018-05-08 10:55:10 +0000

    Script added

---
+++ webapp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python3.6
+sudo apt-get install python3.6-dev
+
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3.6 get-pip.py
+
+sudo apt install python3.6-venv
+
+mkdir Yummy-Recipes
+cd Yummy-Recipes
+
+python3.6 -m venv venv
+git clone https://github.com/HarithJ/Yummy-Recipes-Ch3.git
+source venv/bin/activate
+cd Yummy-Recipes-Ch3
+
+pip install -r requirements.txt
+
+pip install uwsgi
+
+python run.py
